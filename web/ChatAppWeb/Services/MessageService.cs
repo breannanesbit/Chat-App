0ebc88833a23ff2751af9691b18657be5f347da3
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-01-17 23:21:41 -0700

    changed dockerfile

--- web/ChatAppWeb/Services/MessageService.cs
+++ web/ChatAppWeb/Services/MessageService.cs
@@ -15,7 +15,7 @@ namespace ChatAppWeb.Services
 
         public async Task<bool> SendMessage(Message message)
         {
-            var response = await client.PostAsJsonAsync("/api/message/newMessage", message);
+            var response = await client.PostAsJsonAsync("/api/messages/newMessage", message);
             if (response.IsSuccessStatusCode)
             {
                 return true;
@@ -25,7 +25,7 @@ namespace ChatAppWeb.Services
 
         public async Task<List<Message>> GetMessages()
         {
-            var response = await client.GetFromJsonAsync<List<Message>>("/api/message/AllMessage");
+            var response = await client.GetFromJsonAsync<List<Message>>("/api/messages/AllMessage");
             if (response != null)
             {
                 return response;
