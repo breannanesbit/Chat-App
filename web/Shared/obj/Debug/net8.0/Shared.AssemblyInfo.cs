14b5f0e5593657a9dd9967e9e722ac576b995a31
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-01-17 21:13:53 -0700

    retry

--- web/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ web/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e148dbec48e15d0c8a1940bca883eb50c86c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
