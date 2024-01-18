6246b9a95f43530fa4322f2011d022ac31fe2a3a
bobby <bobby@DESKTOP-6V2GR6M>
2024-01-17 23:54:27 -0700

    integration

--- web/Shared/obj/Release/net8.0/Shared.AssemblyInfo.cs
+++ web/Shared/obj/Release/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfb6f1ce06b8d05d15daff75a96b320095b3b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
