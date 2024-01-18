3501ce7708e1e62d5e4760265d05a29709adcc88
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-01-18 14:25:15 -0700

    fix other one

--- web/ChatUnitTests/NewMessageTest.cs
+++ web/ChatUnitTests/NewMessageTest.cs
@@ -1,13 +1,11 @@
-﻿using ChatAppWeb.classes;
-
-namespace ChatUnitTests;
+﻿namespace ChatUnitTests;
 [TestFixture]
 public class NewMessageTest
 {
     [Test]
     public void Add_Message()
     {
-        int messageCount = ChatBox.Messages.Count;
+        //int messageCount = ChatBox.Messages.Count;
         //ChatBox.NewMessage = new Shared.Message { Timestamp = 1/11/2024 1:34:55, Sender = "Riddle", MessageText = "About Time!" };
 
         //ChatBox.;
