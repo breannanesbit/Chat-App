7496768b3a8a3bf84d636796b16a1a4d723ed7cc
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-01-23 15:55:37 -0700

    database talking with api

--- web/init.sql
+++ web/init.sql
@@ -1,18 +1,12 @@
 -- init.sql
-CREATE USER your_username WITH PASSWORD 'your_password';
 
--- Create the database and set the owner
-CREATE DATABASE your_database_name;
-ALTER DATABASE your_database_name OWNER TO your_username;
 \connect your_database_name;
 
-CREATE TABLE IF NOT EXISTS Messages (
-    id SERIAL PRIMARY KEY,
-    sender VARCHAR(255),
-    message_text TEXT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS "Messages" (
+    "Id" SERIAL PRIMARY KEY,
+    "Sender" VARCHAR(255),
+    "MessageText" TEXT,
+    "Timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-INSERT INTO Messages (sender, message_text, timestamp) VALUES ('John', 'Hello, how are you?', DEFAULT);
-INSERT INTO Messages (sender, message_text, timestamp) VALUES ('Alice', 'Hi John! Im doing well.', DEFAULT);
+INSERT INTO "Messages" ("Sender", "MessageText", "Timestamp") VALUES ('John', 'Hello, how are you?', DEFAULT);
+INSERT INTO "Messages" ("Sender", "MessageText", "Timestamp") VALUES ('Alice', 'Hi John! Im doing well.', DEFAULT);
