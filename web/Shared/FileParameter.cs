6541b8c19af4f8569e2957bf830078ee6477cac6
bobby <bobby@DESKTOP-6V2GR6M>
2024-01-23 22:29:01 -0700

    making changes to image

---
+++ web/Shared/FileParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+        public class FileParameter
+        {
+            public byte[] Data { get; }
+            public string FileName { get; }
+
+            public FileParameter(byte[] data, string fileName)
+            {
+                Data = data ?? throw new ArgumentNullException(nameof(data));
+                FileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
+            }
+        }
+
+}
