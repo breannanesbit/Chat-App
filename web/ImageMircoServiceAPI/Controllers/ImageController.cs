eb36c6ef032b07d0559c82b15f9206bf04afbc08
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-01-31 17:44:37 -0700

    added caching started

--- web/ImageMircoServiceAPI/Controllers/ImageController.cs
+++ web/ImageMircoServiceAPI/Controllers/ImageController.cs
@@ -9,34 +9,39 @@ namespace ImageMircoServiceAPI.Controllers
     {
 
 
-    [HttpPost("SaveImage")]
-    public async Task<string> SaveBase64ImageToVolume(string base64Image)
-    {
-        // Check if image compression is enabled via environment variable
-        bool isCompressionEnabled = Environment.GetEnvironmentVariable("IMAGE_COMPRESSION_ENABLED") == "true";
-
-        // Convert base64 string to byte array
-        byte[] imageBytes = Convert.FromBase64String(base64Image);
-
-        if (isCompressionEnabled)
+        [HttpPost("SaveImage")]
+        public async Task<string> SaveBase64ImageToVolume(string base64Image)
         {
-            // Create MagickImage from byte array
-            using (var image = new MagickImage(imageBytes))
+            // Check if image compression is enabled via environment variable
+            bool isCompressionEnabled = Environment.GetEnvironmentVariable("IMAGE_COMPRESSION_ENABLED") == "true";
+            var intervalTime = Environment.GetEnvironmentVariable("TIME_INTERVAL");
+            var parsedIntervalTime = int.Parse(intervalTime);
+            Thread.Sleep(parsedIntervalTime);
+
+
+            // Convert base64 string to byte array
+            byte[] imageBytes = Convert.FromBase64String(base64Image);
+
+            if (isCompressionEnabled)
             {
-                // Perform compression or other image processing if needed
-                image.Quality = 80;
+                // Create MagickImage from byte array
+                using (var image = new MagickImage(imageBytes))
+                {
+                    // Perform compression or other image processing if needed
+                    image.Quality = 80;
 
-                // Convert the MagickImage back to a byte array
-                imageBytes = image.ToByteArray();
+                    // Convert the MagickImage back to a byte array
+                    imageBytes = image.ToByteArray();
+                }
             }
+
+            // Save the byte array to a file
+            var volumePath = "/app/Images";
+            var filePath = Path.Combine(volumePath, "uploaded_image.txt");
+            Directory.CreateDirectory(volumePath);
+            await System.IO.File.WriteAllBytesAsync(filePath, imageBytes);
+            Thread.Sleep(parsedIntervalTime);
+
+            return filePath;
         }
-
-        // Save the byte array to a file
-        var volumePath = "/app/Images";
-        var filePath = Path.Combine(volumePath, "uploaded_image.txt");
-        Directory.CreateDirectory(volumePath);
-        await System.IO.File.WriteAllBytesAsync(filePath, imageBytes);
-
-        return filePath;
     }
-}
