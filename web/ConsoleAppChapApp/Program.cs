c01e09439a2b74c6fc5a448c4b4037e43b6da19c
bobby <bobby@DESKTOP-6V2GR6M>
2024-02-10 14:25:57 -0700

    added metrics(traces, logs, and meters)

--- web/ConsoleAppChapApp/Program.cs
+++ web/ConsoleAppChapApp/Program.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 using Shared;
 using Shared.Data;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Net.Http.Json;
 class Program
 {
+    private static readonly ActivitySource MyActivitySource = new("MyCompany.MyProduct.MyLibrary");
     static async Task Main(string[] args)
     {
+        //Metrics-----------------------------------------------
+            var tracerProvider = Sdk.CreateTracerProviderBuilder()
+               .AddSource("MyCompany.MyProduct.MyLibrary")
+               .AddConsoleExporter()
+               .Build();
+            using (var activity = MyActivitySource.StartActivity("SayHello")) 
+            {
+                activity?.SetTag("foo", 1);
+                activity?.SetTag("bar", "Hello, World!");
+                activity?.SetTag("baz",new int[] {1,2,3});
+                activity?.SetStatus(ActivityStatusCode.Ok);
+            }
+            //Logs
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddOpenTelemetry(options =>
+                {
+                    options.AddConsoleExporter();
+                });
+            });
+            var logger = loggerFactory.CreateLogger<Program>();
+            //Meter
+            Meter myMeter = new("Console.Metrics", "1.0");
+            Counter<long> RequestCounter = myMeter.CreateCounter<long>("RequestCounter");
+            using var meterProvider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter("Console.Metrics")
+                .AddConsoleExporter()
+                .Build();
+        //------------------------------------------------------
         IConfiguration configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -19,6 +56,8 @@ class Program
 
         while (true)
         {   //check the shared database for image files that are only stored on one server.
+            logger.LogInformation("Console checking database for single save images files.");
+
             var files = dbContext.Messages.Where(message => dbContext.MessageContainerLocations
                     .Count(location => location.MessageId == message.Id) == 1 && !string.IsNullOrEmpty(message.ImagePath)).ToList();
 
@@ -28,12 +67,18 @@ class Program
                 {
                     var containerId = message.MessageContainerLocations.First().ContainerLocationId;
                     var baseUri = $"http://image-api-{containerId}:4003/api/Image/getimage/{message.ImagePath}";
+
                     using var httpClient = new HttpClient();
                     try
                     {
                         var image = await httpClient.GetFromJsonAsync<string>(baseUri);
+                        RequestCounter.Add(1, new KeyValuePair<string, object?>("Get Image", HttpMethod.Get));
                         var imageBytes = Convert.FromBase64String(image);
-                        var response = await StoreCopy(imageBytes, containerId);
+                        var response = await StoreCopy(imageBytes, containerId, logger);
+
+                        RequestCounter.Add(1, new KeyValuePair<string, object?>("Post Image", HttpMethod.Post));
+                        logger.LogInformation("Console: Stored image copy");
+
                         //update containers list
                         if (int.TryParse(response.Container, out int cId))
                         { }
@@ -42,8 +87,10 @@ class Program
                             ContainerLocationId = cId,
                         };
                         message.MessageContainerLocations.Add(newContainerLocation);
+                        logger.LogInformation("Console: Added new Container Location");
+
                     }
-                    catch (Exception ex) { }
+                    catch (Exception ex) { logger.LogError($"Console: Problem with Saving image. {ex.Message}"); }
                 }
                 //sleep for an interval of time (env) 5 seconds for dev, 30 prod
                 Thread.Sleep(TimeSpan.FromSeconds(sleepInterval));
@@ -55,6 +102,7 @@ class Program
             }
             //for each instance file, select an image api instance to store a redudnatn copy (at random)
         }
+        tracerProvider.Dispose();
     }
 
     static int GetSleepInterval()
@@ -64,7 +112,7 @@ class Program
         return sleepIntervalSeconds;
     }
 
-    static async Task<Container_path> StoreCopy(byte[] image, int? containerId)
+    static async Task<Container_path> StoreCopy(byte[] image, int? containerId, ILogger logger)
     {
         using var httpClient = new HttpClient();
         if (containerId.HasValue)
@@ -97,7 +145,7 @@ class Program
             }
             catch (Exception ex)
             {
-
+                logger.LogError($"An error occurred while storing the image: {ex.Message}");
             }
         }
         return null;
